ec13560435b230e3a99d1e35ddb6a181d7fb2150
ToukaSama <blacksummoner6659@gmail.com>
2025-03-29 20:59:04 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<blockquote expandable><b>┏━━━━━━━⛩️━━━━━━━┓\n× ɢᴏᴅ : <a href='tg://user?id={OWNER_ID}'>🫨 🫨</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : @Anime_Yugen\n× ᴍᴀɴɢᴀ ᴄʜᴀɴɴᴇʟ : @Manga_Yugen\n× ʏᴜɢᴇɴ ɴᴇᴛᴡᴏᴋ : @YugenNetwork\n┗━━━━━━━⛩️━━━━━━━┛</b></blockquote>",
+            text = f"<blockquote expandable><b>┏━━━━━━━⛩️━━━━━━━┓\n× Oᴡɴᴇʀ : <a href='tg://user?id={6440021089}'>Tσʊкa Sαмα</a>\n× ᴀɴɪᴍᴇ ᴄʜᴀɴɴᴇʟ : @Anime_Sovereign\n× Oɴɢᴏɪɴɢ Aɴɪᴍᴇ : @Ongoing_Sovereign\n× ᴀɴɪᴍᴇ ɪɴ ʜɪɴᴅɪ : @Animez_Hindi_Dub\n┗━━━━━━━⛩️━━━━━━━┛</b></blockquote>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
