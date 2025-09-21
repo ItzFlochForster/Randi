d57b0d43f35a9338bed9cc172ecef303fa778e98
ItzFlochForster <Meowtaro6659@gmail.com>
2025-09-21 19:09:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
-#(©)AnimeXyz
+#(©)AnimeYugen
 
 from aiohttp import web
 from plugins import web_server
-
-import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FSC3, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FSUBS, DB_CHANNEL_ID, PORT
 
 
 name ="""
- BY MIKEY FROM TG
+ BY Voat FROM TG
 """
 
 
@@ -35,70 +33,38 @@ class Bot(Client):
         await super().start()
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
-
-        if FORCE_SUB_CHANNEL:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-                self.invitelink = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
-                sys.exit()
-        if FORCE_SUB_CHANNEL2:
-            try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
-                    link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
-                self.invitelink2 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
-                sys.exit()
-
-        if FSC3:
-            try:
-                link = (await self.get_chat(FSC3)).invite_link
-                if not link:
-                    await self.export_chat_invite_link(FSC3)
-                    link_a = (await self.create_chat_invite_link(chat_id=FSC3, creates_join_request=True)).invite_link 
-                self.invitelink3 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FSC3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FSC3}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/weebs_support for support")
-                sys.exit()
         try:
-            db_channel = await self.get_chat(CHANNEL_ID)
+            db_channel = await self.get_chat(DB_CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id = db_channel.id, text = "Testing Message by @VOATcb")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the DB_CHANNEL_ID Value, Current Value {DB_CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Yugen_Bots_Support for support")
             sys.exit()
-
+        
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/VOATcb")
         self.LOGGER(__name__).info(f""" \n\n       
                                                    
                   
-                                 
+             chadarmod                    
 
-  ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
- / __/ _ \|   \| __| __| |  |_ _\ \/ / _ )/ _ \_   _/ __|
-| (_| (_) | |) | _|| _|| |__ | | >  <| _ \ (_) || | \__ \
- \___\___/|___/|___|_| |____|___/_/\_\___/\___/ |_| |___/
-                                                         
+
+██╗░░░██╗██╗░░░██╗░██████╗░███████╗███╗░░██╗
+╚██╗░██╔╝██║░░░██║██╔════╝░██╔════╝████╗░██║
+░╚████╔╝░██║░░░██║██║░░██╗░█████╗░░██╔██╗██║
+░░╚██╔╝░░██║░░░██║██║░░╚██╗██╔══╝░░██║╚████║
+░░░██║░░░╚██████╔╝╚██████╔╝███████╗██║░╚███║
+░░░╚═╝░░░░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚══╝
+
+            ██████╗░░█████╗░████████╗░██████╗
+            ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
+            ██████╦╝██║░░██║░░░██║░░░╚█████╗░
+            ██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
+            ██████╦╝╚█████╔╝░░░██║░░░██████╔╝
+            ╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░                                                         
  
                                                                         
                                                                       
@@ -116,3 +82,4 @@ class Bot(Client):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
             
+        
